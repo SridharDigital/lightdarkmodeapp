ae34e1d8f5e7bf72f056cd2a22e29e9d03b49b15
Sreedharan Namboodiri C <sridhardigital22@gmail.com>
2023-06-15 13:28:02 +0000

    second Commit

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -6,21 +6,27 @@ class LightDarkMode extends Component {
 
   render() {
     const {isDark} = this.state
-    let output;
-    if(isDark) {
-       output = (
-       <div className="light-theme">
-            <h1 className="dark-heading">Click to Change Mode</h1>
-            <button className="button">Dark Mode</button>
-        </div>)
-      } else {
-          output = (
-          <div className="dark-theme">
-            <h1 className="light-heading">Click to Change Mode</h1>
-            <button className="button">Light Mode</button>
-        </div>)
-      }
+    let output
+    if (isDark) {
+      output = (
+        <div className="light-theme">
+          <h1 className="dark-heading">Click to Change Mode</h1>
+          <button className="button" type="button">
+            Dark Mode
+          </button>
+        </div>
+      )
+    } else {
+      output = (
+        <div className="dark-theme">
+          <h1 className="light-heading">Click to Change Mode</h1>
+          <button className="button" type="button">
+            Light Mode
+          </button>
+        </div>
+      )
+    }
     return {output}
+  }
 }
-
 export default LightDarkMode
