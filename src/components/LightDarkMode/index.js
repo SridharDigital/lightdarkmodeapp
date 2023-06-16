bb2fe1866088060c9461bd3c610df5508a27877b
Sreedharan Namboodiri C <sridhardigital22@gmail.com>
2023-06-16 06:22:49 +0000

    updated bg container

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -4,29 +4,42 @@ import './index.css'
 class LightDarkMode extends Component {
   state = {isDark: false}
 
+  onClickUpdate = () => {
+    this.setState(prevState => {
+      let status
+      if (prevState.isDark) {
+        status = {isDark: false}
+      } else {
+        status = {isDark: true}
+      }
+      return status
+    })
+  }
+
   render() {
     const {isDark} = this.state
     let output
     if (isDark) {
       output = (
-        <div className="light-theme">
+        <div className="card light-theme">
           <h1 className="dark-heading">Click to Change Mode</h1>
-          <button className="button" type="button">
+          <button className="button" type="button" onClick={this.onClickUpdate}>
             Dark Mode
           </button>
         </div>
       )
     } else {
       output = (
-        <div className="dark-theme">
+        <div className="card dark-theme">
           <h1 className="light-heading">Click to Change Mode</h1>
-          <button className="button" type="button">
+          <button className="button" type="button" onClick={this.onClickUpdate}>
             Light Mode
           </button>
         </div>
       )
     }
-    return {output}
+    return <div className="bg-container">{output}</div>
   }
 }
+
 export default LightDarkMode
